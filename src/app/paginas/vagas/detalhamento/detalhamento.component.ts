8fe36de1cf4383e96530eba95fd8b8f386c37104
DevSamuel06 <samuelribeiro.filho16@gmail.com>
2024-06-06 11:06:18 -0300

    Detalhamento de vagas

--- src/app/paginas/vagas/detalhamento/detalhamento.component.ts
+++ src/app/paginas/vagas/detalhamento/detalhamento.component.ts
@@ -9,4 +9,16 @@ export class DetalhamentoComponent {
   readonly cifraoImg: string = "../../../../assets/img/cifrao.png";
   readonly localImg: string = "../../../../assets/img/pin-de-localizacao.png";
 
+  vagas = [
+    {
+      tipo: "Desenvolvedor Front-end", 
+      local: "São Paulo, SP", 
+      descricao: "Estamos procurando um desenvolvedor front-end talentoso para se juntar à nossa equipe. Você será responsável por criar interfaces de usuário incríveis.",
+      beneficio: ["Vale alimentação", "Vale transporte", "PLR"],
+      salario: "R$ 6.000 - R$ 8.000", 
+      requisitos: ["Conhecimentos em Angular", "typescript" ,  "bootstrap"],
+      dataPubli: "26/05/2024"
+    }
+  ];
+
 }
